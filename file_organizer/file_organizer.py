6263965ea92e52bcb6314553302dfff798e8cf14
leisenhour2 <leisenhour2@gmail.com>
2025-09-03 21:47:30 -0400

    Update "config.json" and "file_organizer.py"
    "config.json" now includes 2 new keys for "Input"/"Output" and now "file_organizer.py" can take "Input" folder(Ex. Desktop, Documents, etc.) assigned via "config.json" and sort them in "Output" folder assigned in "config.json"

--- file_organizer/file_organizer.py
+++ file_organizer/file_organizer.py
@@ -2,48 +2,40 @@ from pathlib import Path
 import json, shutil
 
 PARENT_DIR = Path(__file__).resolve().parent
-CONFIGURATION = PARENT_DIR / "config.json"
 
-with open(CONFIGURATION, "r") as config_file:
-    config = json.load(config_file)
-
-INPUT = PARENT_DIR / "INPUT"
-OUTPUT = PARENT_DIR / "OUTPUT"
+with open(PARENT_DIR / "config.json", "r") as jsonfile:
+    file_dir = json.load(jsonfile)
 
 
-def creates_folder(folder) -> Path:
+def create_folder(folder) -> Path:
     f = Path(folder)
     f.mkdir(parents=True, exist_ok=True)
-    return f
+    return Path(f)
 
 
-# Make sure folders exist
-creates_folder(INPUT)
-creates_folder(OUTPUT)
-creates_folder(OUTPUT / "Misc")
-creates_folder(OUTPUT / "Files")
-for key in config.keys():
-    creates_folder(OUTPUT / key)
+INPUT = create_folder(file_dir["Input"])
+OUTPUT = create_folder(file_dir["Output"])
 
+MISC = create_folder(OUTPUT / "Misc")
+FILES = create_folder(OUTPUT / "Files")
+FILETYPES = file_dir["FileTypes"]
 
-# Sort files
-for file in INPUT.iterdir():
-    if file.is_file():
-        ext = file.suffix.lower()
+for key in FILETYPES.keys():
+    create_folder(OUTPUT / key)
 
-        # No extension → Files
+for item in INPUT.iterdir():
+    if item.is_dir():
+        shutil.move(str(item), OUTPUT / "Misc" / item.name)
+    if item.is_file():
+        ext = item.suffix.lower()
         if not ext:
-            shutil.move(str(file), OUTPUT / "Files" / file.name)
+            shutil.move(str(item), OUTPUT / "Files" / item.name)
             continue
-
-        # Try to match extension in config
-        moved = False
-        for key, value in config.items():
+        for key, value in FILETYPES.items():
+            moved = False
             if ext in value:
-                shutil.move(str(file), OUTPUT / key / file.name)
+                shutil.move(str(item), OUTPUT / key / item.name)
                 moved = True
                 break
-
-        # If no match at all → Misc
         if not moved:
-            shutil.move(str(file), OUTPUT / "Misc" / file.name)
+            shutil.move(str(item), OUTPUT / "Misc" / item.name)
